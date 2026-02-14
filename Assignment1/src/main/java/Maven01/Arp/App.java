6c66a16b7c21b02d02cd4d30d8c2ef4a03e4526f
Arpit Yadav <arpityadav@Arpits-MacBook-Air-4.local>
2026-02-14 22:53:26 +0530

    Initial clean commit - Maven Assignments 1 & 2

---
+++ Assignment1/src/main/java/Maven01/Arp/App.java
@@ -0,0 +1,20 @@
+package Maven01.Arp;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String args) {
+		System.out.println("fdsds");
+	}
+	    }
